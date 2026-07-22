c4e25115ea10f2341f9e573f024713d2b89fb047
rajeshrceaDA <rajeshrcea70@gmail.com>
2026-07-22 09:25:24 +0530

    Create 01_database_exploration.sql

---
+++ sql/01_database_exploration.sql
@@ -0,0 +1,35 @@
+/*
+=============================================================
+Project      : AdventureWorks SQL Data Analysis
+Analysis     : Database Exploration
+Author       : Rajesh Kumar
+Database     : AdventureWorks
+SQL Engine   : MySQL
+
+Objective:
+Explore the AdventureWorks database structure and identify
+the key business tables required for sales analysis.
+
+=============================================================
+*/
+
+-- ==========================================================
+-- Business Question 1
+-- Explore all available tables in the AdventureWorks database.
+-- ==========================================================
+
+SHOW TABLES;
+
+-- ==========================================================
+-- Business Question 2
+-- Examine the structure of the SalesOrderHeader table.
+-- ==========================================================
+
+DESCRIBE SalesOrderHeader;
+
+-- ==========================================================
+-- Business Question 4
+-- Examine the structure of the Product table.
+-- ==========================================================
+
+DESCRIBE Product;
